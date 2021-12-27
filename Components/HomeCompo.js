17f0337ced4ad3abdf822caad8e35067ab026781
Arun-solanki-301 <arunsolanki@excellencetechnologies.in>
2021-12-27 15:53:03 +0530

    test

--- Components/HomeCompo.js
+++ Components/HomeCompo.js
@@ -1,73 +1,105 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { Text, View, StyleSheet, CheckBox } from "react-native";
+import { Text, View, StyleSheet, TouchableOpacity } from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
+// import { TouchableOpacity } from "react-native-gesture-handler";
 
 const Output = ({ navigation }) => {
   const [Data, setData] = useState([]);
-  // const [checkbox , setcheckbox] = useState(false)
+  const [checkedStatus , setCheckedStatus] = useState(false);
 
   useEffect(() => {
     pollData();
   }, []);
 
-  const pollData = () => {
+  const pollData = async () => {
     const Url = "https://secure-refuge-14993.herokuapp.com/list_polls";
-    axios.get(Url).then((res) => {
-      setData(res.data.data);
-    });
+    let res = await axios.get(Url);
+    setData(res.data.data);
   };
-  const [isSelected, setIsSelected] = useState(false);
 
+  // provide ids ........
+  let newArr = [];
+  const checkboxPress = (id, option) => {
+    Data.forEach((ques) => {
+      if (ques._id === id) {
+        // console.log(id, ques, option);
+        newArr.push({
+          id: id,
+          option_text: option,
+        });
+        // console.log(newArr)
+      }
+    });
 
-  
-  // provide id ........
-  let count = 0;
+    // Data?.forEach((curr) => {
+    //   curr.options.forEach((elem) => {
+    //     if (id === elem.id) {
+    //       if (!newArr.includes(id)) {
+    //         newArr.push(id);
+    //       } else newArr.filter((value) => value.id !== id);
+    //     }
+    //     console.log(newArr, "aaaaaaaaaaaaa");
+    //   });
+    // });
+  };
+
+  // const getChecked = (id, option) => {
+  //   let checked = false
+  //   newArr.forEach(val=>{
+  //     if(val.id === id){
+  //       if(val.option_text === option){
+  //         checked = true
+  //       }
+  //     }
+  //   })
+  //   console.log(id, option, checked)
+  //   return checked
+  // }
+  // console.log(Data)
+
+  let newlyArr = [];
+  var count = 0;
   Data?.forEach((curr)=>{
     curr.options.forEach((elem)=>{
-        elem.id = count;
-        count++;
+      elem.id = count;
+      count++
     })
   })
-
-  let newArr = [];
-  const checkboxPress = (id) => {
+  //hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh
+  const handleCheckedBox = (element)=>{
     Data?.forEach((curr)=>{
       curr.options.forEach((elem)=>{
-        console.log(elem , id)
-        if(id === elem.id){
-          if(newArr.includes(id))
-          newArr.push(id)
-          else newArr.filter((value)=>value.id !== id)
+        if(elem.id === element.id){
+          if(!newlyArr.includes(element.id)){
+            newlyArr.push(element.id)
+          }
         }
-
-        
-        console.log(newArr ,"aaaaaaaaaaaaa")
-        // else{
-        //   setIsSelected(false)
-        // }
+        else newlyArr.filter((value) => value !== element.id)
       })
     })
-    }
-    console.log(newArr , Data ,"dhwkdkdkwjdkwjdkwj")
+    // setCheckedStatus(!checkedStatus)
+    console.log(newlyArr)
+  }
+
+
+
   return (
+    <ScrollView>
     <View>
-      {Data?.map((curr , outerIndex) => {
+      {Data?.map((curr, i) => {
         return (
-          <View key={curr.id}>
-            <Text style={styles.TitleOfPoll}>{curr?.title}</Text>
+          <View key={i}>
+            <Text style={styles.TitleOfPoll}>{curr.title}</Text>
             <View>
-              {curr.options.map((opcurr , innerIndex) => {
+              {curr.options.map((opcurr, i) => {
                 return (
-                  <View style={styles.container}>
+                  <View key={i} style={styles.container}>
                     <View style={styles.checkboxContainer}>
-                      <CheckBox
-                        value={newArr.includes(opcurr.id)}
-                        onValueChange={()=>checkboxPress(opcurr.id)}
-                        style={styles.checkbox} 
-                      />
-                      <Text style={styles.optionsOfPoll}>
-                        {opcurr.option}
-                      </Text>
+                      <TouchableOpacity style={checkedStatus ? styles.checkboxTrue : styles.checkboxOpt} onPress={()=>handleCheckedBox(opcurr)}> 
+                        <Text></Text>
+                        </TouchableOpacity>
+                        <Text>{opcurr.option}</Text>
                     </View>
                   </View>
                 );
@@ -77,6 +109,7 @@ const Output = ({ navigation }) => {
         );
       })}
     </View>
+    </ScrollView>
   );
 };
 const styles = StyleSheet.create({
@@ -89,13 +122,17 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     textAlign: "center",
   },
-  optionsOfPoll: { color: "red", fontSize: 22, fontWeight: "500" , textAlign : "center" },
+  optionsOfPoll: {
+    color: "red",
+    fontSize: 22,
+    fontWeight: "500",
+    textAlign: "center",
+  },
   optionContainer: {
     display: "flex",
     flexDirection: "row",
     // marginHorizontal: 15,
     // padding: 20,
-    
   },
   checkboxOpt: {
     height: 25,
@@ -103,36 +140,32 @@ const styles = StyleSheet.create({
     borderColor: "#1a73e8",
     borderWidth: 2,
     marginRight: 10,
-    borderRadius: "50%",
+    borderRadius: 5,
   },
-  // checkboxTrue: {
-  //   height: 25,
-  //   width: 25,
-  //   borderColor: "#1a73e8",
-  //   borderWidth: 2,
-  //   marginRight: 10,
-  //   borderRadius: "50%",
-  //   backgroundColor: "green",
-  // },
+  checkboxTrue: {
+    height: 25,
+    width: 25,
+    borderColor: "#1a73e8",
+    borderWidth: 2,
+    marginRight: 10,
+    borderRadius: 5,
+    backgroundColor: "green",
+  },
   container: {
     flex: 1,
-    alignItems : "flex-start",
-marginHorizontal : 25
-
+    alignItems: "flex-start",
+    marginHorizontal: 25,
   },
   checkboxContainer: {
     flexDirection: "row",
     marginBottom: 20,
-    justifyContent : "space-between"
+    justifyContent: "space-between",
   },
   checkbox: {
     alignSelf: "center",
     marginRight: 15,
-    
-
+    backgroundColor: "blue"
   },
-
-  
 });
 
 export default Output;
