4c3f86e8a8b7cc658943cafc409075aea8c0e1a2
Arun-solanki-301 <arunsolanki@excellencetechnologies.in>
2021-12-27 11:28:16 +0530

    second

--- Components/HomeCompo.js
+++ Components/HomeCompo.js
@@ -5,7 +5,7 @@ import { Text, View, StyleSheet, CheckBox } from "react-native";
 const Output = ({ navigation }) => {
   const [Data, setData] = useState([]);
   // const [checkbox , setcheckbox] = useState(false)
-  var count = 0;
+
   useEffect(() => {
     pollData();
   }, []);
@@ -16,36 +16,57 @@ const Output = ({ navigation }) => {
       setData(res.data.data);
     });
   };
-  const [isSelected, setSelection] = useState(false);
-//   const checkboxPress = () => {
-//     setcheckbox(true);
-//   };
+  const [isSelected, setIsSelected] = useState(false);
 
-  console.log(Data);
 
+  
+  // provide id ........
+  let count = 0;
+  Data?.forEach((curr)=>{
+    curr.options.forEach((elem)=>{
+        elem.id = count;
+        count++;
+    })
+  })
+
+  let newArr = [];
+  const checkboxPress = (id) => {
+    Data?.forEach((curr)=>{
+      curr.options.forEach((elem)=>{
+        console.log(elem , id)
+        if(id === elem.id){
+          if(newArr.includes(id))
+          newArr.push(id)
+          else newArr.filter((value)=>value.id !== id)
+        }
+
+        
+        console.log(newArr ,"aaaaaaaaaaaaa")
+        // else{
+        //   setIsSelected(false)
+        // }
+      })
+    })
+    }
+    console.log(newArr , Data ,"dhwkdkdkwjdkwjdkwj")
   return (
     <View>
-      {Data?.map((curr) => {
+      {Data?.map((curr , outerIndex) => {
         return (
           <View key={curr.id}>
             <Text style={styles.TitleOfPoll}>{curr?.title}</Text>
             <View>
-              {curr.options.map((opcurr) => {
+              {curr.options.map((opcurr , innerIndex) => {
                 return (
-                  // <View style = {styles.optionContainer}>
-                  // <TouchableOpacity style={checkbox ?styles.checkboxTrue:styles.checkboxOpt}  onPress={()=>checkboxPress()}><Text></Text></TouchableOpacity>
-                  // <Text style={styles.optionsOfPoll}>{opcurr.option}</Text>
-                  // </View>
                   <View style={styles.container}>
                     <View style={styles.checkboxContainer}>
                       <CheckBox
-                        value={isSelected}
-                        onValueChange={setSelection}
-                        style={styles.checkbox}
-                        id={count + 1}
+                        value={newArr.includes(opcurr.id)}
+                        onValueChange={()=>checkboxPress(opcurr.id)}
+                        style={styles.checkbox} 
                       />
-                      <Text style={styles.label}>
-                        Do you like React Native?
+                      <Text style={styles.optionsOfPoll}>
+                        {opcurr.option}
                       </Text>
                     </View>
                   </View>
@@ -68,12 +89,13 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     textAlign: "center",
   },
-  optionsOfPoll: { color: "red", fontSize: 22, fontWeight: "500" },
+  optionsOfPoll: { color: "red", fontSize: 22, fontWeight: "500" , textAlign : "center" },
   optionContainer: {
     display: "flex",
     flexDirection: "row",
-    marginHorizontal: 15,
-    padding: 20,
+    // marginHorizontal: 15,
+    // padding: 20,
+    
   },
   checkboxOpt: {
     height: 25,
@@ -83,30 +105,34 @@ const styles = StyleSheet.create({
     marginRight: 10,
     borderRadius: "50%",
   },
-  checkboxTrue: {
-    height: 25,
-    width: 25,
-    borderColor: "#1a73e8",
-    borderWidth: 2,
-    marginRight: 10,
-    borderRadius: "50%",
-    backgroundColor: "green",
-  },
+  // checkboxTrue: {
+  //   height: 25,
+  //   width: 25,
+  //   borderColor: "#1a73e8",
+  //   borderWidth: 2,
+  //   marginRight: 10,
+  //   borderRadius: "50%",
+  //   backgroundColor: "green",
+  // },
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
+    alignItems : "flex-start",
+marginHorizontal : 25
+
   },
   checkboxContainer: {
     flexDirection: "row",
     marginBottom: 20,
+    justifyContent : "space-between"
   },
   checkbox: {
     alignSelf: "center",
+    marginRight: 15,
+    
+
   },
-  label: {
-    margin: 8,
-  },
+
+  
 });
 
 export default Output;
